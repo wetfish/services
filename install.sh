b70de79e717561c46e77ed492cf313e31fad3d2a
Rachel Fish <itsrachelfish@gmail.com>
2015-02-25 02:10:26 -0700

    Integrated wetfish login

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Clone the wetfish login project
+git clone https://github.com/wetfish/login.git
+
+# Install node dependencies
+cd login
+npm install
--- webserver.js
+++ webserver.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+var login = require("./login/sdk/server/wetfish-login");
+var config = require("./config/secret");
+
+login.init(config.login);
 
 server.listen(2303);
 console.log("IRC services web server started");
 
 app.use(express.static(__dirname + '/static'));
+
+app.get('/login', function(req, res)
+{
+    login.verify(req.query.token, function(verified)
+    {
+        if(verified.status == "success")
+            res.end("Here's the data you allowed to be shared:\n\n" + JSON.stringify(verified.data, null, 4));
+        else
+            res.end("There was an error!\n\n" + verified.message);
+    });
+});
