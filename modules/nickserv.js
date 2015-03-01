d00ac8bf570f7fbc451c67c3c4cc08b5da7428e0
Rachel Fish <itsrachelfish@gmail.com>
2015-02-28 22:16:05 -0700

    Restructured nickserv module to support multiple event listeners

--- modules/nickserv.js
+++ modules/nickserv.js
@@ -4,33 +4,9 @@ var login = require("../login/sdk/server/wetfish-login");
 
 var nickserv =
 {
-    events: ['raw', 'message'],
-    commands: ['register', 'auth', 'login', 'host'],
-
-    // Event handlers
-    raw: function(input)
-    {
-        if(input.command == "rpl_youreoper")
-        {
-            nickserv.init();
-        }
-    },
-
-    message: function(from, to, message)
-    {
-        message = message.split(" ");
-        var command = message.shift();
-
-        // Only accept PMs
-        if(to != client.nick) return;
-
-        // If this is a valid command
-        if(nickserv.commands.indexOf(command) > -1)
-        {
-            // Call the handler function
-            nickserv['_'+command](from, message);
-        }
-    },
+    //
+    // General functions
+    ////////////////////////////////////////
 
     // Initialize services after authentication
     init: function()
@@ -41,9 +17,92 @@ var nickserv =
         client.send('sanick', client.nick, 'NickServ');
     },
 
-    // User commands
+    // Bind and unbind events
+    bind: function()
+    {
+        for(var i = 0, l = nickserv.events.client.length; i < l; i++)
+        {
+            var event = nickserv.events.client[i];
+            client.addListener(event, nickserv["client_" + event]);
+        }
+
+        for(var i = 0, l = nickserv.events.redis.length; i < l; i++)
+        {
+            var event = nickserv.events.redis[i];
+            core.model.redis.addListener(event, nickserv["redis_" + event]);
+        }
+    },
+
+    unbind: function()
+    {
+        for(var i = 0, l = nickserv.events.client.length; i < l; i++)
+        {
+            var event = nickserv.events.client[i];
+            client.removeListener(event, nickserv["client_" + event]);
+        }
+
+        for(var i = 0, l = nickserv.events.redis.length; i < l; i++)
+        {
+            var event = nickserv.events.redis[i];
+            core.model.redis.removeListener(event, nickserv["redis_" + event]);
+        }
+    },
+
+
+    //
+    // Event handlers
+    ////////////////////////////////////////
+
+    events:
+    {
+        client: ['raw', 'message'],
+        redis: ['ready', 'message']
+    },
+
+    client_raw: function(input)
+    {
+        if(input.command == "rpl_youreoper")
+        {
+            nickserv.init();
+        }
+    },
+
+    client_message: function(from, to, message)
+    {
+        message = message.split(" ");
+        var command = message.shift();
+
+        // Only accept PMs
+        if(to != client.nick) return;
+
+        // If this is a valid command
+        if(nickserv.commands.indexOf(command) > -1)
+        {
+            // Call bot command handler function
+            nickserv['_'+command](from, message);
+        }
+    },
+
+    redis_ready: function()
+    {
+        console.log(arguments);
+    },
+
+    redis_message: function()
+    {
+        console.log(arguments);
+    },
+
+    //
+    // Bot commands
+    ////////////////////////////////////////
+    
+    commands: ['register', 'login', 'ghost', 'host'],
+
     _register: function(user, message)
     {
+        // Check if username is already registered
+        
         // Generate a unique token for this request
         core.model.token.set(user, "register", function(token)
         {
@@ -52,55 +111,43 @@ var nickserv =
         });
     },
 
-    _auth: function(user, message)
+    _login: function(user, message)
     {
-        var token = message.shift();
-
-        console.log("Verifying specified token...");
-        login.verify(req.query.token, function(verified)
+        // Check if username is actually registered
+        
+        // Generate a unique token for this request
+        core.model.token.set(user, "login", function(token)
         {
-            if(verified.status == "success")
-            {
-                core.model.redis.get(user, function(error, response)
-                {
-                    client.say(user, "Your account has now been verified. Would you like to use the password ");
-                });
-            }
-            else
-            {
-
-            }
+            // Notify the user
+            client.say(user, "Logging in as "+user+"! Please visit https://services.wetfish.net/token/"+token+" to authorize this action.");
         });
     },
 
-    _login: function()
+    _ghost: function(user, message)
     {
-        console.log("Yay a password");
+        var target = message.shift();
+        // Check if target is a registered name
+
+        if(target)
+        {
+            // Generate a unique token for this request
+            core.model.token.set(user, "ghost", function(token)
+            {
+                // Notify the user
+                client.say(user, "User "+target+" will be disconnected! Please visit https://services.wetfish.net/token/"+token+" to authorize this action.");
+            });
+        }
+        else
+        {
+            client.say(user, "Error! You must specify a username to disconnect.");
+        }
     },
 
     _host: function()
     {
+        // Check if user is logged in
         console.log("ur very crative");
     },
-
-    // General helpers
-    bind: function()
-    {
-        for(var i = 0, l = nickserv.events.length; i < l; i++)
-        {
-            var event = nickserv.events[i];
-            client.addListener(event, nickserv[event]);
-        }
-    },
-
-    unbind: function()
-    {
-        for(var i = 0, l = nickserv.events.length; i < l; i++)
-        {
-            var event = nickserv.events[i];
-            client.removeListener(event, nickserv[event]);
-        }
-    }
 }
 
 module.exports =
